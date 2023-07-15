0bca4cd5f7654b77a9109a249a4f5465e0a26b17
Your Name <you@example.com>
2023-07-15 14:54:16 +0200

    Add comment.

--- examples/chat-stream-func/src/main.rs
+++ examples/chat-stream-func/src/main.rs
@@ -20,6 +20,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
         
     let result = client.chat().create_stream(request).await;
 
+    // For reasons not documented in OpenAI docs / OpenAPI spec,
+    // the response of streaming call is different and doesn't include all the same fields.
+
+    // From Rust docs on print: https://doc.rust-lang.org/std/macro.print.html
+    //
+    //  Note that stdout is frequently line-buffered by default so it may be necessary
+    //  to use io::stdout().flush() to ensure the output is emitted immediately.
+    //
+    //  The print! macro will lock the standard output on each call.
+    //  If you call print! within a hot loop, this behavior may be the bottleneck of the loop.
+    //  To avoid this, lock stdout with io::stdout().lock():
+
     match result {
         Ok(mut chat_stream) => {
             let mut lock = stdout();
